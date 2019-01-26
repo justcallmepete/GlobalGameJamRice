1e12246ecf4d1ebb6dc3b08c7149dd8a7c3aba57
takkiemon <takkie91@hotmail.com>
2019-01-26 19:26:53 +0100

    add cliffdeath prefab (collision and script)

---
+++ GlobalGameJamRice/Assets/Tak Man/CliffDeathScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CliffDeathScript : MonoBehaviour {
+
+    public void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.GetComponent<Movement>())
+        {
+            collision.gameObject.transform.localPosition = new Vector3(-.25f, 5f, 0.5f);
+        }
+    }
+}
